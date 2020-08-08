7b684da71d76a9f346220e39fc91ef7020627de4
arevathi69 <66195488+arevathi69@users.noreply.github.com>
2020-08-08 19:27:25 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter two numbers");
+	scanf("%d%d",&a,&b);
+	printf("\nThe numbers before swapping is %d %d ",a,b);
+	a=a-b;
+	b=a+b;
+	a=b-a;
+	printf("\nThe numbers after swapping is %d %d ",a,b);
+	return 0;
+}
